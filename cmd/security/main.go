7eae87a4d9534c299f3b78da223411fc5273ffc6
Jason Scheirer <jason.scheirer@gmail.com>
2025-06-01 10:35:05 -0700

    Find and get now implement Just Enough Functionality

--- cmd/security/main.go
+++ cmd/security/main.go
@@ -1,8 +1,44 @@
 // build: -darwin
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type CommandFunc func([]string)
+
+var commands map[string]CommandFunc
+
+func registerCommand(commandName string, command CommandFunc) {
+	if commands == nil {
+		commands = make(map[string]CommandFunc)
+	}
+	commands[commandName] = command
+}
+
+func help() {
+	fmt.Printf("%s: Emulates a subset of macOS' security command\n", os.Args[0])
+
+	for subcommand, commandFunc := range commands {
+		fmt.Printf("%s %s:\n", os.Args[0], subcommand)
+		commandFunc([]string{"-help"})
+	}
+}
 
 func main() {
-	fmt.Println("security")
+	if len(os.Args) > 1 {
+		commandName := os.Args[1]
+		args := os.Args[2:]
+
+		cmdFunc := commands[commandName]
+
+		if cmdFunc != nil {
+			cmdFunc(args)
+		} else {
+			help()
+		}
+	} else {
+		help()
+	}
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,14 @@ go 1.24.3
 
 require golang.design/x/clipboard v0.7.0
 
+require github.com/zalando/go-keyring v0.2.6
+
 require (
+	al.essio.dev/pkg/shellescape v1.5.1 // indirect
+	github.com/danieljoos/wincred v1.2.2 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.6.0 // indirect
 	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 )
